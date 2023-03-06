d43b8dd8b37972a1401c6495a2fbc6aa4107b363
Delyc <d.twizeyima@alustudent.com>
2023-03-06 17:52:02 +0200

    Feat: Applying reviews

--- src/Questions.js
+++ src/Questions.js
@@ -1,73 +1,16 @@
-import React, { useEffect, useState } from "react";
-const Question = (props) => {
-  const allAnswer = [
-    ...props.value.incorrect_answers,
-    props.value.correct_answer,
-  ];
-  const [selected, setSelected] = React.useState(null);
+import { useEffect, useState } from "react";
+import Question from "./Question";
 
-  function styleQuestion() {
-    if (props.checkAns) {
-      if (selected === props.value.correct_answer) {
-        return "bg-green-900  text-white border-none";
-      } else {
-        return "bg-red-900  text-white border-none opacity-50";
-      }
-    } else {
-      return "bg-violet-200 text-slate-700 border-none";
-    }
-  }
-  return (
-    <div className=" border-b py-2">
-      <h2
-        className="text-xl font-semibold text-slate-700"
-        dangerouslySetInnerHTML={{ __html: props.value.question }}
-      ></h2>
-      <div className="flex gap-2 lg:flex-nowrap flex-wrap my-3">
-        {allAnswer.sort().map((value, index) => {
-          return (
-            <div
-              key={index}
-              onClick={() => {
-                !props.checkAns && setSelected(value);
-                props.setAllClientAnswer((prev) => {
-                  prev[props.id] = {
-                    ...prev[props.id],
-                    isCollect:
-                      value === props.value.correct_answer ? true : false,
-                  };
-                  return prev;
-                });
-              }}
-              className={`rounded-xl border-2 py-1 px-5 cursor-pointer ${
-                value === selected && styleQuestion()
-              }
-              ${
-                props.checkAns && props.value.correct_answer === value
-                  ? "bg-green-900 text-white border-none"
-                  : "border-slate-700"
-              }
-              `}
-            >
-              {value}
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-};
 export default function Questions() {
   const [data, setData] = useState([]);
   const [countCorrect, setCountCorrect] = useState(0);
   const [checkAns, setCheckAns] = useState(false);
   const [allClientAnswer, setAllClientAnswer] = useState(data);
-  function feactQuestion() {
+  function fetchQuestion() {
     fetch("https://opentdb.com/api.php?amount=5")
       .then((res) => res.json())
       .then((data) => {
         setData(data.results);
-        console.log("data", data);
         setAllClientAnswer(
           data.results.map((obj) => ({ ...obj, isCollect: false }))
         );
@@ -75,12 +18,12 @@ export default function Questions() {
   }
   function playAgain() {
     setData([]);
-    setCountCorrect(0); // reset count correct
-    setCheckAns(false); // reset check answer
-    feactQuestion(); // fetch new question
+    setCountCorrect(0); 
+    setCheckAns(false); 
+    fetchQuestion(); 
   }
   useEffect(() => {
-    feactQuestion();
+    fetchQuestion();
   }, []);
 
   return (
